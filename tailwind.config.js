72952d4425abf321310140245ffa86775ab588e8
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-17 12:04:19 +0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
       fontFamily: {
         sans: ['Poppins', 'system-ui', 'sans-serif'],
       },
+      spacing: {
+        '30': '7.5rem',
+        '18': '4.5rem',
+      },
       colors: {
         primary: {
           DEFAULT: 'hsl(var(--color-primary))', 
@@ -64,6 +68,7 @@ export default {
         lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
         xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
         '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        'nav': '0 2px 10px rgba(0, 0, 0, 0.05)',
         inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
         none: 'none',
       },
