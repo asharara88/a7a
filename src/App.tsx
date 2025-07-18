0cbb6bb880bd31803299b7c613decab034590ef8
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-18 16:59:05 +0000

    ppaa lala

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ErrorBoundary from './components/ui/ErrorBoundary';
 import Layout from './components/layout/Layout';
