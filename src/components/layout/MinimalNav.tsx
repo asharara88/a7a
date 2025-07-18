2aa22077420546806d5bb21f2e6cf141979e8d91
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-18 17:01:14 +0400

    Fix MinimalNav Component Definition

--- src/components/layout/MinimalNav.tsx
+++ src/components/layout/MinimalNav.tsx
@@ -1,3 +1,17 @@
 
 
+import React from 'react';
+
+interface MinimalNavProps {
+  isDarkMode: boolean;
+}
+
+const MinimalNav: React.FC<MinimalNavProps> = ({ isDarkMode }) => {
+  return (
+    <nav className="w-full">
+      {/* Navigation content will go here */}
+    </nav>
+  );
+};
+
 export default MinimalNav;
