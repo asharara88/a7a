6048c3de2a3276c8778dbf06ce998e5bdfe8a7eb
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-18 20:03:18 +0400

    Fix React import error in SupplementsPage.tsx

--- src/pages/SupplementsPage.tsx
+++ src/pages/SupplementsPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect, useState, useMemo } from "react";
 import { CheckCircle, AlertCircle, Info, Loader2, Shield } from "lucide-react";
 import Breadcrumbs from '../components/ui/Breadcrumbs';
