8db784f3718af569b62748ead8f710d212143386
asharara88 <ahmed.m.sharara@gmail.com>
2025-07-18 14:22:45 +0000

    iiiii

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import NutritionDashboardPage from './pages/NutritionDashboardPage';
 import SupplementRecommendationsPage from './pages/SupplementRecommendationsPage';
 import MetabolismPage from './pages/MetabolismPage';
 import BioclockPage from './pages/BioclockPage';
+import MyPlatePage from './pages/MyPlatePage';
 
 function App() {
   return (
